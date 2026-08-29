339863733bf1c1ace8875ceebd36ebd8aba09309
veierblat <gilbert.klee@education.lu>
2026-08-29 10:38:48 +0200

    Add URLs and notes to activities in activities.js

--- newzealand/data/activities.js
+++ newzealand/data/activities.js
@@ -8,22 +8,36 @@ const activities = [
     place: "Queenstown",
     name: "Skyline Gondola",
     date: "16.11.2026",
+
     lat: -45.0265,
     lon: 168.6537,
+
     type: "Gondel / Aussicht",
+
     duration: "ca. 2–3 h",
-    note: ""
+
+    note: "",
+
+    url: "",
+    guidePage: null
   },
 
   {
     place: "Queenstown",
     name: "Ausflug nach Glenorchy",
     date: "17.11.2026",
+
     lat: -44.8506,
     lon: 168.3880,
+
     type: "Tagesausflug",
+
     duration: "ganztägig",
-    note: ""
+
+    note: "",
+
+    url: "",
+    guidePage: null
   },
 
 
@@ -35,23 +49,37 @@ const activities = [
     place: "Wānaka",
     name: "Puzzling World",
     date: "19.11.2026",
+
     lat: -44.6960,
     lon: 169.1660,
+
     type: "Besichtigung",
+
     duration: "ca. 1,5–2 h",
-    note: ""
+
+    note: "",
+
+    url: "",
+    guidePage: null
   },
 
   {
     place: "Wānaka",
     name: "Mount Iron Track",
     date: "20.11.2026",
+
     lat: -44.684,
     lon: 169.162,
+
     type: "Wanderung",
+
     distance: "ca. 4,5 km",
     duration: "ca. 1,5–2 h",
-    note: "Alternative: Roys Peak Track"
+
+    note: "Alternative: Roys Peak Track",
+
+    url: "",
+    guidePage: null
   },
 
 
@@ -63,30 +91,19 @@ const activities = [
     place: "Blue Pools / Cameron Flat",
     name: "Blue Pools Track",
     date: "21.11.2026",
+
     lat: -44.164,
     lon: 169.278,
+
     type: "Wanderung",
+
     distance: "ca. 3 km",
     duration: "ca. 1–1,5 h",
-    note: "Nach Ankunft am Cameron Flat Campsite"
-  },
 
-  
+    note: "Nach Ankunft am Cameron Flat Campsite",
 
-  // -------------------------
-  // ship creek
-  // -------------------------
-
-  {
-    place: "Ship Creek",
-    name: "Kahikatea Swamp Forest Walk",
-    date: "22.11.2026",
-    lat: -43.758282,
-    lon: 169.147283,
-    type: "Aussicht",
-    distance: "ca. 1 km",
-    duration: "ca. 20 min",
-    note: "Ass e Parking do"
+    url: "",
+    guidePage: null
   },
 
 
@@ -98,12 +115,19 @@ const activities = [
     place: "Fox Glacier",
     name: "Lake Matheson",
     date: "23.11.2026",
+
     lat: -43.443,
     lon: 169.970,
+
     type: "Wanderung",
+
     distance: "ca. 4,4 km",
     duration: "ca. 1,5 h",
-    note: ""
+
+    note: "",
+
+    url: "",
+    guidePage: null
   },
 
 
@@ -115,13 +139,21 @@ const activities = [
     place: "Hokitika",
     name: "Hokitika Gorge",
     date: "25.11.2026",
+
     lat: -42.945,
     lon: 171.023,
+
     type: "Wanderung / Aussicht",
+
     distance: "ca. 2 km",
     duration: "ca. 1 h",
-    note: ""
+
+    note: "",
+
+    url: "",
+    guidePage: null
   },
+
 
   // -------------------------
   // KAITERITERI
@@ -131,13 +163,21 @@ const activities = [
     place: "Kaiteriteri",
     name: "Abel Tasman Wanderung",
     date: "29.11.2026",
+
     lat: -41.037032,
     lon: 173.017513,
+
     type: "Wanderung / Aussicht",
+
     distance: "ca. 11-12 km",
     duration: "ca. 3-4 h",
-    note: "Aquataxi 9:00 op Bark Bay, Wanderung vun BarkBay op Anchorage, Aquataxi zeréck 16:00"
+
+    note:
+      "Aquataxi 9:00 op Bark Bay, Wanderung vun BarkBay " +
+      "op Anchorage, Aquataxi zeréck 16:00",
+
+    url: "",
+    guidePage: null
   }
 
-  
 ];
