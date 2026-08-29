44cadfcaaab354a740ed02f9a34058d40a3d509b
veierblat <gilbert.klee@education.lu>
2026-08-29 10:33:17 +0200

    Enhance camping data with camperMate links and corrections
    Updated camping information for New Zealand, including added camperMate URLs and corrected notes.

--- newzealand/data/campings.js
+++ newzealand/data/campings.js
@@ -3,16 +3,13 @@
  * CAMPINGPLÄTZE – NEUSEELAND 2026
  * ============================================================
  *
- * Zusätzliche Angaben zum WoMo-Reiseführer:
+ * Zusätzliche Angaben:
  *
- * guideNumber = Campingnummer im Reiseführer
- * guidePage   = Seite im Reiseführer
+ * guideNumber   = Campingnummer im WoMo-Reiseführer
+ * guidePage     = Seite im WoMo-Reiseführer
+ * camperMateUrl = direkter Link zum Camping in CamperMate
  *
- * guidePage ist zunächst überall null.
- * Die Seitenzahlen können später einfach ergänzt werden.
- *
- * Hat ein Camping keine bekannte Campingnummer,
- * wird guideNumber weggelassen.
+ * Alle Angaben sind optional.
  *
  * ============================================================
  */
@@ -41,16 +38,17 @@ const campings = [
 
     url: "https://www.ashburtondc.govt.nz/community-facilities/facilities%2C-venues-and-room-bookings/council-campgrounds",
 
+    camperMateUrl: "",
+
     guideNumber: 64,
     guidePage: 58
   },
 
 
   /*
-   * Backup zu Rakaia Gorge
-   *
-   * BUP = Backup
-   * WoMo-Reiseführer Nr. 67
+   * ----------------------------------------------------------
+   * PEEL FOREST – BACKUP
+   * ----------------------------------------------------------
    */
 
   {
@@ -63,9 +61,11 @@ const campings = [
 
     facilities: "DOC Campsite",
 
-    note: "Backup fir Rakaia Gorge Camp.",
+    note: "Backup für Rakaia Gorge Camp.",
 
     url: "https://www.doc.govt.nz/",
+
+    camperMateUrl: "",
 
     guideNumber: 67,
     guidePage: 61
@@ -93,6 +93,8 @@ const campings = [
 
     url: "https://lakesedgeholidaypark.co.nz/",
 
+    camperMateUrl: "https://campermate.com/en/location/new-zealand/can/mackenzie/paid-campsites/lakes-edge-holiday-park/383dd32e-7930-4010-8a70-7409be7d4068",
+
     guideNumber: 71,
     guidePage: 64
   },
@@ -114,12 +116,13 @@ const campings = [
     facilities: "WC",
 
     note:
-      "Ganz simpel. " +
-      "Keng Telefonnummer Keng Rezeptioun." +
-      "Schéi roueg beim Séi, mä Kräsi, keng Beem." ,
+      "Einfache Alternative bei Lake Tekapo. " +
+      "Keine verlässlich publizierte direkte Campingplatz-Telefonnummer.",
 
     url:
       "https://www.mackenzie.govt.nz/services/parks-venues-and-recreation/camping",
+
+    camperMateUrl: "https://campermate.com/en/location/new-zealand/can/mackenzie/paid-campsites/lake-mcgregor-campervan-site/ec147ae6-e6fb-4aed-97a2-06147cf40903",
 
     guideNumber: 72,
     guidePage: 66
@@ -150,6 +153,8 @@ const campings = [
     url:
       "https://www.doc.govt.nz/parks-and-recreation/places-to-go/canterbury/places/aoraki-mount-cook-national-park/things-to-do/white-horse-hill-campground/",
 
+    camperMateUrl: "https://campermate.com/en/location/new-zealand/can/mackenzie/low-cost-campground/white-horse-hill-campground/80a15ccc-38e9-49e9-988e-d10600ec82b1",
+
     guideNumber: 76,
     guidePage: 72
   },
@@ -177,6 +182,8 @@ const campings = [
     url:
       "https://top10.co.nz/park/omarama-top-10-holiday-park/",
 
+    camperMateUrl: "https://campermate.com/en/location/new-zealand/can/waitaki/paid-campsites/omarama-top-10-holiday-park/c86aa4ed-592d-4dcc-8f98-8cd986098794",
+
     guideNumber: 121,
     guidePage: 101
   },
@@ -202,6 +209,8 @@ const campings = [
     phone: "+64 3 442 9447",
 
     url: "https://www.camp.co.nz/",
+
+    camperMateUrl: "https://campermate.com/en/location/new-zealand/ota/queenstown/paid-campsites/queenstown-holiday-park-creeksyde/addb6409-ddfd-440f-abeb-d7d6aca4f428",
 
     guideNumber: 244,
     guidePage: 175
@@ -229,6 +238,8 @@ const campings = [
 
     url:
       "https://tasmanholidayparks.com/south-island/otago/wanaka/",
+
+    camperMateUrl: "",
 
     guidePage: null
   },
@@ -258,6 +269,8 @@ const campings = [
     url:
       "https://www.doc.govt.nz/parks-and-recreation/places-to-go/otago/places/mount-aspiring-national-park/things-to-do/campsites/cameron-flat-campsite/",
 
+    camperMateUrl: "https://campermate.com/en/location/new-zealand/ota/wanaka/lowcost-campground/cameron-flat-campsite/154ff523-b71b-4cb2-8f7b-4395f6d686d1",
+
     guideNumber: 259,
     guidePage: 189
   },
@@ -283,6 +296,8 @@ const campings = [
     phone: "+64 3 751 0821",
 
     url: "https://www.fghp.co.nz/",
+
+    camperMateUrl: "https://campermate.com/en/location/new-zealand/s/west-coast/paid-campsites/fox-glacier-top-10-holiday-park/8372f2d8-ef65-4f48-b307-08f8d4cbb1ff",
 
     guideNumber: 273,
     guidePage: 200
@@ -310,6 +325,8 @@ const campings = [
 
     url: "https://rainforest.nz/",
 
+    camperMateUrl: "https://campermate.com/en/location/new-zealand/s/west-coast/paid-campsites/rainforest-retreat-holiday-park/57e5279b-a5f0-4053-b9fc-56feb1d08cfd",
+
     guideNumber: 274,
     guidePage: 203
   },
@@ -336,6 +353,8 @@ const campings = [
 
     url: "https://www.pjl.nz/",
 
+    camperMateUrl: "https://campermate.com/en/location/new-zealand/wtc/west-coast/paid-campsites/shining-star-beachfront-accommodation/af8975e1-c41e-4c63-86d1-bd5be641d57f",
+
     guideNumber: 282,
     guidePage: 214
   },
@@ -361,6 +380,8 @@ const campings = [
     phone: "+64 27 539 9666",
 
     url: "https://www.murchisonmotorhomepark.co.nz/",
+
+    camperMateUrl: "https://campermate.com/en/location/new-zealand/s/nelson-tasman/paid-campsites/murchison-motorhome-park/aa2c4ee5-7a8a-472b-b2a7-bce25196ea8e",
 
     guideNumber: 393,
     guidePage: 270
@@ -389,6 +410,8 @@ const campings = [
     url:
       "https://www.visitmurchison.nz/riverside-holiday-park.html",
 
+    camperMateUrl: "",
+
     guideNumber: 392,
     guidePage: 270
   },
@@ -415,6 +438,8 @@ const campings = [
 
     url: "https://www.bethanypark.co.nz/",
 
+    camperMateUrl: "https://campermate.com/en/location/new-zealand/tas/nelson-tasman/paid-campsites/bethany-park-holiday-park/eb495f59-1d8f-429c-b320-be458f72c997",
+
     guidePage: null
   },
 
@@ -439,6 +464,8 @@ const campings = [
     phone: "+64 3 527 8010",
 
     url: "https://kaiteriteri.co.nz/",
+
+    camperMateUrl: "https://campermate.com/en/location/new-zealand/tas/nelson-tasman/paid-campsites/kaiteriteri-recreation-reserve/225f8fa1-b2e2-4106-8afa-a4bde69b3fef",
 
     guideNumber: 424,
     guidePage: 280
@@ -466,6 +493,8 @@ const campings = [
 
     url: "https://www.havelockholidaypark.kiwi/",
 
+    camperMateUrl: "https://campermate.com/en/location/new-zealand/s/marlborough/paid-campsites/havelock-holiday-park/332d2d6d-f63b-409e-bb4a-ea9bae399079",
+
     guideNumber: 458,
     guidePage: 308
   },
@@ -492,6 +521,8 @@ const campings = [
 
     url: "https://www.wavescampsite.co.nz/",
 
+    camperMateUrl: "https://campermate.com/en/location/new-zealand/mbh/marlborough/paid-campsites/waves-campsite/c21460fd-9eb2-4964-a2dd-32868c6e7fde",
+
     guidePage: null
   },
 
@@ -517,6 +548,8 @@ const campings = [
 
     url:
       "https://www.southbrightonholidaypark.co.nz/",
+
+    camperMateUrl: "https://campermate.com/en/location/new-zealand/s/christchurch/paid-campsites/south-brighton-holiday-park/ab23286f-d081-49a5-9a99-d37eb97719de",
 
     guideNumber: 33,
     guidePage: 40
